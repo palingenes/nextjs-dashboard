64d6dfeabd7b2c57e18dd44404e733b3cba65f05
wzy <drampalingensis@163.com>
2026-07-27 18:24:18 +0800

    chapter 14

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,8 +1,11 @@
 "use server";
+
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 import postgres from "postgres";
 
@@ -115,4 +118,23 @@ export async function deleteInvoice(id: string, prevState: State) {
   }
   revalidatePath("/dashboard/invoices");
   return { message: null };
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
